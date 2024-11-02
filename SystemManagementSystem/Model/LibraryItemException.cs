109d0e4603ad9d65d5d9922e1d9e91c3036fc114
Togrul12345 <togrultb-ab205@code.edu.az>
2024-11-02 16:01:55 +0400

    committed

---
+++ SystemManagementSystem/Model/LibraryItemException.cs
@@ -0,0 +1,19 @@
+﻿
+namespace SystemManagementSystem.LibraryCatalog
+{
+    [Serializable]
+    internal class LibraryItemException : Exception
+    {
+        public LibraryItemException()
+        {
+        }
+
+        public LibraryItemException(string? message) : base(message)
+        {
+        }
+
+        public LibraryItemException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ SystemManagementSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SystemManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
