45dc9cb4457ff8142ea0e49584947c611303a271
Togrul12345 <togrultb-ab205@code.edu.az>
2024-11-06 11:19:02 +0400

    committed

---
+++ DrTask/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http.Json;
+
+namespace DrTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         //   string path = @"C:\Users\Togrul\source\repos\DrTask\DrTask";
+         //   Directory.CreateDirectory(path + @"\Model");
+         //   Directory.CreateDirectory(path + @"\Data");
+         //File.Create(path + @"\Data" + " jsonData.json");
+            HttpClient httpClient = new HttpClient();
+            string newPath = "https://jsonplaceholder.typicode.com/posts";
+            str= httpClient.GetStringAsync(newPath).Result.;
+            
+           
+          
+
+
+        }
+    }
+}
