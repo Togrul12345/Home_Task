8c3f602dd846d861e886efe346567482b6a5f4d5
Togrul12345 <togrultb-ab205@code.edu.az>
2024-11-19 22:45:29 +0400

    committed

---
+++ MvcTask1/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcTask1.Models;
+
+namespace MvcTask1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            Student student = new Student()
+            {
+                Id = 1,
+                Name = "Fizaret",
+            };
+            Student student2 = new Student()
+            {
+                Id = 2,
+                Name = "MirYusif",
+            };
+            List<Student> students = new List<Student>() { student, student2 };
+            return View(students);
+        }
+    }
+}
